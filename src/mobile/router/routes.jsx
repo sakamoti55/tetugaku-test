d7f4cf3c2b3c7ebf2d38b3add2ef6d76549570fb
sakamoti55 <kohseimeimei@gmail.com>
2025-02-17 10:00:02 +0900

    a

--- src/mobile/router/routes.jsx
+++ src/mobile/router/routes.jsx
@@ -1,8 +1,8 @@
 import { Navigate } from "react-router-dom";
 import RootLayouts from "@m/components/layouts/RootLayout.jsx";
 import MainLayouts from "@m/components/layouts/MainLayout.jsx";
-import Loading from "@m/components/pages/Loading/Loading.jsx";
-import Home from "@m/components/pages/Main/Home.jsx";
+import Loading from "@m/components/pages/loading/Loading.jsx";
+import Home from "@m/components/pages/main/Home.jsx";
 
 // ルート定義を簡略化
 const route = (path, element, children = []) => ({ path, element, children });
