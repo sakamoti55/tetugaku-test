52f1f02c50b73e4775f1d073dc9a3e499a3b6e8c
sakamoti55 <kohseimeimei@gmail.com>
2025-02-17 10:12:41 +0900

    aaaa

--- src/mobile/components/pages/main/Home.jsx
+++ src/mobile/components/pages/main/Home.jsx
@@ -1,15 +1,15 @@
 import { useState, useEffect } from "react";
 import Network from "@m/components/organisms/network/Network.jsx";
 import BottomSheet from "@m/components/organisms/bottom-sheet/BottomSheet.jsx";
-import { fetchData } from "@m/api/fetchData";
+// import { fetchData } from "@m/api/fetchData";
+import processedData from "/create_json/output_data/processed_data";
 
 export default function Home() {
-  const [processedData, setProcessedData] = useState(null);
   const [nodeId, setNodeId] = useState(5);
 
-  useEffect(() => {
-    fetchData().then((data) => setProcessedData(data));
-  }, []);
+  // useEffect(() => {
+  //   fetchData().then((data) => setProcessedData(data));
+  // }, []);
 
   return (
     <>
